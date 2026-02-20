f220ab685b3f3ee73c56932eca6368336437c1d2
chits <chits@gmail.com>
2026-02-20 12:55:54 +0530

    updated product

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -9,7 +9,7 @@ function Products() {
 
   useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_API_URL}`/api/products)
+      .get(`${process.env.REACT_APP_API_URL}/api/products`)
       .then(res => setProducts(res.data))
       .catch(err => console.log(err));
   }, []);
